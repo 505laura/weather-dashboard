85162b14eef86b57096906b76339b91314845d72
505laura <106522200+505laura@users.noreply.github.com>
2022-09-24 23:49:55 +1000

    Store/load recently searched cities to/from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,16 @@
 // API key for openweathermap
 const key = 'e45a73480beb9e2bd5f0d93532fd4564';
+
+// Storing data about the cities that have been searched recently
+let recentlySearched = [];
+
+// Get recently searched cities from local storage
+function getRecents() {
+    recentlySearched = JSON.parse(localStorage.getItem('recentlySearched') || '[]').slice(0, 5);
+}
+
+// Save recently searched cities to local storage
+function saveRecents() {
+    recentlySearched = recentlySearched.slice(0, 5);
+    localStorage.setItem('recentlySearched', JSON.stringify(recentlySearched));
+}
