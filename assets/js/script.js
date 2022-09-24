dcfa137c4ce13bc23d243549acb036d37797eded
505laura <106522200+505laura@users.noreply.github.com>
2022-09-24 23:50:24 +1000

    Add function to populate weather info card

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,16 @@ function saveRecents() {
     recentlySearched = recentlySearched.slice(0, 5);
     localStorage.setItem('recentlySearched', JSON.stringify(recentlySearched));
 }
+
+// Fill in one card with the data for that day
+function populateCard(card, data) {
+    const today = $(card);
+    today.children('.date').text(moment(data.dt_txt).format('MMM Do'));
+    // Get the icon for the weather from the openweathermap url
+    today.children('.card-img-bottom').attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@4x.png`);
+    today.children('.weather').children('span').text(data.weather[0].main);
+
+    today.children('.temp').children('span').text(data.main.temp);
+    today.children('.wind').children('span').text(data.wind.speed);
+    today.children('.humidity').children('span').text(data.main.humidity);
+}
